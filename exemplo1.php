d8f7962dd6bac06e69186ad71c5d80d5b5dbf6cc
hengustavo <amadeu090806@gmail.com>
2024-02-20 11:48:17 -0300

    Ola mundo php

---
+++ exemplo1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+            background-color: red;
+            font-size: 28px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Amadeu</h1>
+    <div>
+    <?php 
+        $nota = 10.5;
+        echo "<div>Nota final:$nota</div>";
+    ?>
+    </div>
+</body>
+</html>
---
+++ ola.php
@@ -0,0 +1,4 @@
+<?php
+    echo "Ola ifsp";
+    echo "<h1>Amadeu</h1>";
+?>
